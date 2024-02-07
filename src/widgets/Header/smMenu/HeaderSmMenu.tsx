7b828629766e4d43fa60ac4879e84f1f654896b4
trepegorka <egorpotapko@mail.ru>
2024-02-07 14:50:43 +0100

    Sm menu fix links, state

--- src/widgets/Header/smMenu/HeaderSmMenu.tsx
+++ src/widgets/Header/smMenu/HeaderSmMenu.tsx
@@ -2,6 +2,8 @@ import {classNames} from 'src/shared/helpers/classNames/classNames'
 import cls from './HeaderSmMenu.module.scss'
 import {useNavbarClose} from "src/app/providers/NavBarProvider/lib/useNavbarClose.tsx";
 import {Theme, useTheme} from "src/app/providers/ThemeProvider";
+import {HashLink} from "react-router-hash-link";
+import {RoutePath} from "src/app/providers/router/config/routeConfig.tsx";
 
 interface HeaderSmMenuProps {
     className?: string
@@ -9,23 +11,23 @@ interface HeaderSmMenuProps {
 
 export const HeaderSmMenu = ({className}: HeaderSmMenuProps) => {
     const {theme} = useTheme()
-    const {isClose} = useNavbarClose()
+    const {isClose,changeClose} = useNavbarClose()
 
     return (
         <div className={classNames(cls.header__smMenu, {[cls.header__smMenu__active]: !isClose, [cls.dark]: (theme===Theme.DARK)}, [className])}>
             <div className={cls.header__smMenuContent}>
                 <ul className={cls.header__smMenuLinks}>
                     <li className={cls.header__smMenuLink}>
-                        <a href="./">Home</a>
+                        <HashLink to={RoutePath.main.concat('#home')} onClick={changeClose}>Home</HashLink>
                     </li>
                     <li className={cls.header__smMenuLink}>
-                        <a href="./#about">About</a>
+                        <HashLink to={RoutePath.main.concat('#about')} onClick={changeClose}>About</HashLink>
                     </li>
                     <li className={cls.header__smMenuLink}>
-                        <a href="./#projects">Projects</a>
+                        <HashLink to={RoutePath.main.concat('#projects')} onClick={changeClose}>Projects</HashLink>
                     </li>
                     <li className={cls.header__smMenuLink}>
-                        <a href="./#contact">Contact</a>
+                        <HashLink to={RoutePath.main.concat('#contact')} onClick={changeClose}>Contact</HashLink>
                     </li>
                 </ul>
             </div>
