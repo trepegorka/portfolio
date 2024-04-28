3e6b9af22646ffd2c4686d4f427732629fb77e70
trepegorka <egorpotapko@mail.ru>
2024-04-28 18:13:55 +0200

    fix skills

--- src/widgets/AboutSection/AboutContentSkills/AboutContentSkills.tsx
+++ src/widgets/AboutSection/AboutContentSkills/AboutContentSkills.tsx
@@ -11,12 +11,11 @@ export const AboutContentSkills = ({className}: AboutContentSkillsProps) => {
         <div className={classNames(cls.AboutContentSkills, {}, [className])}>
             <h3 className={cls.aboutContentTitle}>My Skills</h3>
             <div className={cls.skills}>
-                <SkillContainer>JavaScript</SkillContainer>
-                <SkillContainer>React.js</SkillContainer>
-                <SkillContainer>Redux</SkillContainer>
+                <SkillContainer>Next.js</SkillContainer>
                 <SkillContainer>Typescript</SkillContainer>
-                <SkillContainer>HTML5</SkillContainer>
-                <SkillContainer>CSS3</SkillContainer>
+                <SkillContainer>PostgreSQL</SkillContainer>
+                <SkillContainer>Tailwind CSS</SkillContainer>
+                <SkillContainer>Redux(RTK)</SkillContainer>
                 <SkillContainer>SASS</SkillContainer>
                 <SkillContainer>Webpack</SkillContainer>
                 <SkillContainer>Vite</SkillContainer>
@@ -25,15 +24,11 @@ export const AboutContentSkills = ({className}: AboutContentSkillsProps) => {
                 <SkillContainer>Loki</SkillContainer>
                 <SkillContainer>GIT</SkillContainer>
                 <SkillContainer>CI/CD</SkillContainer>
-                <SkillContainer>NPM</SkillContainer>
-                <SkillContainer>UI</SkillContainer>
-                <SkillContainer>Code splitting</SkillContainer>
             </div>
             <h3 className={classNames(cls.aboutContentTitle, {}, [cls.secondaryTitle])}>Secondary skills</h3>
             <div className={cls.skills}>
-                <SkillContainer>Java</SkillContainer>
+                <SkillContainer>Java SE</SkillContainer>
                 <SkillContainer>Python</SkillContainer>
-                <SkillContainer>SQL</SkillContainer>
                 <SkillContainer>Bootstrap</SkillContainer>
                 <SkillContainer>ANT Design</SkillContainer>
                 <SkillContainer>Webflow</SkillContainer>
@@ -41,14 +36,10 @@ export const AboutContentSkills = ({className}: AboutContentSkillsProps) => {
             <h3 className={classNames(cls.aboutContentTitle, {}, [cls.secondaryTitle])}>In study</h3>
             <div className={cls.skills}>
                 <SkillContainer>Advanced Typescript</SkillContainer>
-                <SkillContainer>Automation</SkillContainer>
                 <SkillContainer>Cypress</SkillContainer>
                 <SkillContainer>Docker</SkillContainer>
                 <SkillContainer>GraphQL</SkillContainer>
-                <SkillContainer>Next.js</SkillContainer>
-                <SkillContainer>Tailwind CSS</SkillContainer>
                 <SkillContainer>React Native</SkillContainer>
-                <SkillContainer>PostgreSQL</SkillContainer>
                 <SkillContainer>Mongo DB</SkillContainer>
             </div>
         </div>
